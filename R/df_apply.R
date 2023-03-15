76ac9b61d27c68486b2b346a7081a15e3920e12d
ashill1470 <ashill1470@gmail.com>
2023-03-15 16:52:57 -0400

    Added documentation

--- R/df_apply.R
+++ R/df_apply.R
@@ -1,3 +1,22 @@
+
+#' Apply a function to specific columns of a data frame
+#'
+#' @param .data A data frame.
+#' @param .fun A function that takes in a vector and returns a vector.
+#' @param .filter A function that takes in a vector and returns a logical vector.
+#' @param ... Additional arguments to pass to .fun and .filter.
+#'
+#' @return A data frame that is a modified version of .data
+#' @export
+#'
+#' @examples
+#' df <- data.frame(
+#'   A = c(1, 2, 3),
+#'   B = c(4, 5, 6),
+#'   C = c("foo", "bar", "baz"))
+#' my_fun <- function(x) x * 2
+#' my_filter <- function(x) is.numeric(x)
+#' df_apply(df, my_fun, my_filter)
 df_apply <- function(.data, .fun, .filter, ...) {
   modified_columns <- lapply(.data, function(x) {
     if (.filter(x)){
