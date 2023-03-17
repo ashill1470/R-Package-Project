e39b8e53d68cbe3fedea796721c72dcb99323460
ashill1470 <ashill1470@gmail.com>
2023-03-17 16:40:00 -0400

    Added dataset documentation and population documentation.

---
+++ R/RPackProj.R
@@ -0,0 +1,26 @@
+#' Dataset Documentation
+#'
+#'
+#' Collection of world wide standard populations (in millions) used for age standardization
+#'
+#' The variables are as follows:
+#'
+#' \itemize{
+#'    \item age_group : The age group of interest
+#'    \item 1940 U.S. Std Million : US Population in 1940
+#'    \item 1950 U.S. Std Million : US Population in 1950
+#'    \item 1960 U.S. Std Million : US Population in 1960
+#'    \item 1970 U.S. Std Million : US Population in 1970
+#'    \item 1980 U.S. Std Million : US Population in 1980
+#'    \item 1990 U.S. Std Million : US Population in 1990
+#'    \item 1991 Canadian Std Million : Canadian Population in 1991
+#'    \item 1996 Canadian Std Million : Canadian Population in 1996
+#'    \item 2000 U.S. Std Million : US Population in 2000
+#' }
+#'
+#' @name population
+#' @docType data
+#' @author Matthias Kleine, Uploaded by Andrew Hill
+#' @references https://www.kaggle.com/datasets/matthiaskleine/standard-populations-dataset?resource=download
+#' @usage data(population)
+NULL
