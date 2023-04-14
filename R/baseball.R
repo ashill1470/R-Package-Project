b3c361b6d442f0cc36781a07379d67b8476ac69d
ashill1470 <ashill1470@gmail.com>
2023-04-13 22:41:50 -0400

    Added Baseball dataset and started plotly vignette

---
+++ R/baseball.R
@@ -0,0 +1,18 @@
+#' Baseball Team Data
+#'
+#' Data on baseball teams dating back to 1871.
+#'
+#' @format ## `baseball`
+#' A data frame with 2,805 rows and 48 columns. Column descriptions can be
+#' accessed at the source. Some important columns are:
+#' \describe{
+#'   \item{yearID}{Year}
+#'   \item{teamID, name}{Name of team}
+#'   \item{G}{Games Played}
+#'   \item{W, L}{Wins, Losses}
+#'   \item{AB, H, double, triple, HR, BB, SO}{At-Bats, Hits, Doubles, Triples, Home Runs, Walks, Strikeouts}
+#' }
+#' @source <https://baseballsavant.mlb.com/#>
+#' @docType data
+#' @author BaseballSavant Maintainers, Found and uploaded by Andrew Hill
+#' @usage data(baseball)
